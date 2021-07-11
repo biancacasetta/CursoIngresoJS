d6d6a17a5a24b54b721513a535d5b126d7de716a
biancacasetta <86834800+biancacasetta@users.noreply.github.com>
2021-07-11 19:07:13 -0300

    EJ02 resuelto

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,15 @@
 /*
+Bianca Casetta
+Enunciado EJ02:
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
 
+	var nombre;
+	var mensaje;
+
+	nombre = prompt("Ingrese su nombre", "Natalia natalia");
+	mensaje = "Su nombre es: "+ nombre;
+	alert(mensaje);
+}
